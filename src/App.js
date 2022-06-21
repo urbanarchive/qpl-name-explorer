d3959697973bb4901bdf1ad99b1bbbd730d03c40
Matt Gardner <wilburnforce@gmail.com>
2022-06-21 15:53:46 -0500

    Tailwind setup; basic flex layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col h-full w-full max-h-screen">
+      <div className="flex flex-col sm:flex-row h-full overflow-scroll">
+        <div className="basis-1/2 relative">
+          Test
+        </div>
+        <div className="basis-1/2 border-l-8 max-h-full overflow-scroll">
+          Test
+        </div>
+      </div>
     </div>
   );
 }
