0310fa7a60491669c41b2c678c0a54b6b60ed72d
Matt Gardner <wilburnforce@gmail.com>
2022-06-23 10:22:49 -0500

    Basic layout and add ETL

---
+++ etl.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const AIRTABLE = {
+  domain: 'https://api.airtable.com',
+  path: '/v0/appS1fzGsI76K6IPO/Collections Metadata',
+  maxRecords: -1,
+  view: 'U/A view',
+  key: process.env.AIRTABLE_API_KEY,
+}
+const endpoint = `${AIRTABLE.domain}${AIRTABLE.path}?maxRecords=${AIRTABLE.maxRecords}&view=${AIRTABLE.view}`;
+const dataFolder = '/data';
+const pathToData = (ext = '.json') => path.join(__dirname, dataFolder, 'monuments') + ext;
+
+async function getData() {
+  try {
+    const { data: { records } } = await axios(endpoint, {
+      headers: {
+        'Authorization': `Bearer ${AIRTABLE.key}`,
+      },
+    });
+
+    return records.map(r => {
+      return { id: r.id, ...r.fields };
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// execute and persist data
+getData() // no top level await... yet
+  .then((data) => {
+    // persist data
+    fs.writeFileSync(path.resolve(pathToData('.json')), JSON.stringify(data, null, 2));
+    fs.writeFileSync(path.resolve(pathToData('.geojson')), JSON.stringify(jsonToGeoJson(data)));
+  })
+  .catch(e => console.log(e));
+
+function jsonToGeoJson(json) {
+  return  {
+    type: 'FeatureCollection',
+    features: json
+      .filter(row => !!row['Georef (U/A)'])
+      .map(row => {
+        return {
+          type: 'Feature',
+          properties: {
+            ...row
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: row['Georef (U/A)'].split(',').reverse().map(c => parseFloat(c.trim())),
+          },
+        }
+      })
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ function App() {
   return (
     <div className="flex flex-col h-full w-full max-h-screen">
       <div className="flex flex-col sm:flex-row h-full overflow-scroll">
-        <div className="basis-1/2 relative">
-          Test
+        <div className="basis-3/5 relative">
+          Map
         </div>
-        <div className="basis-1/2 border-l-8 max-h-full overflow-scroll">
-          Test
+        <div className="basis-2/5 border-l-8 max-h-full overflow-scroll">
+          List/Detail
         </div>
       </div>
     </div>
