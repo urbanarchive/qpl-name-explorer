2e593881fd7cd75ed9353189abdf0954536a5fe3
Matt Gardner <wilburnforce@gmail.com>
2022-07-07 11:15:04 -0500

    Updated Logo

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -6,7 +6,7 @@ function Header({ className }) {
   return <>
     <div className={`flex gap-4 p-4 ${className}`}>
       <Link to="/" className="flex w-10 h-8 items-center">
-        <img className='w-8 h-8 bg-red-100 p-1 rounded-md' src={qplLogo} alt="QPL Logo" />
+        <img className='w-8 h-8 bg-red-100 rounded-md' src={qplLogo} alt="QPL Logo" />
       </Link>
       <div className="grow h-8">
         <input className='w-full h-full bg-gray-200 rounded-md p-2' type="text" placeholder='Search QPL Stories'></input>
