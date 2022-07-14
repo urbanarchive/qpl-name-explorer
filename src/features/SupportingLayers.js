2de3093557ffbceeb928b5783437eb1d59799d87
Matt Gardner <wilburnforce@gmail.com>
2022-07-14 18:02:14 -0500

    Toggle supporting layers

--- src/features/SupportingLayers.js
+++ src/features/SupportingLayers.js
@@ -1,6 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import mapLayers from '../features/images/map-layers.png';
+
+function getVisibility(state) {
+  if (state === undefined) return 'visible';
+  return state ? 'visible' : 'none';
+}
 
 export default function SupportingLayers({ map }) {
+  const [supportingLayersToggleVisible, setVisibility] = useState(false);
+  const [layersState, setLayerState] = useState({
+    cds: false,
+    ntas: false,
+  });
+
   useEffect(() => {
     async function fetchData() {
       const data = {
@@ -8,10 +20,14 @@ export default function SupportingLayers({ map }) {
         ntas: await (await fetch('/data/static/ntas.geojson')).json(),
       };
   
-      // setData(data);
       map.addSource('supporting-cds', {
         type: 'geojson',
         data: data.cds,
+      });
+
+      map.addSource('supporting-ntas', {
+        type: 'geojson',
+        data: data.ntas,
       });
 
       map.addLayer({
@@ -25,7 +41,28 @@ export default function SupportingLayers({ map }) {
             "stops": [[11, 1], [16, 3]],
           },
         },
+        layout: {
+          'visibility': getVisibility(layersState.cds),
+        },
       });
+
+      map.addLayer({
+        id: 'supporting-ntas',
+        source: 'supporting-ntas',
+        type: 'line',
+        paint: {
+          'line-color': '#00007a',
+          'line-opacity': 0.6,
+          'line-width': {
+            stops: [[11, 1], [16, 3]]
+          }
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round',
+          'visibility': getVisibility(layersState.ntas),
+        }
+      })
 
       map.addLayer({
         id: 'supporting-cds-labels',
@@ -46,19 +83,72 @@ export default function SupportingLayers({ map }) {
           ],
           'text-size': {
             stops: [[11, 12], [14, 16]]
-          }
+          },
+          'visibility': getVisibility(layersState.cds),
         }
-      })
+      });
+
+      map.addLayer({
+        id: 'supporting-ntas-labels',
+        source: 'supporting-ntas',
+        type: 'symbol',
+        minzoom: 11,
+        paint: {
+          'text-color': '#626262',
+          'text-halo-color': '#FFFFFF',
+          'text-halo-width': 2,
+          'text-halo-blur': 2
+        },
+        layout: {
+          'text-field': "{NTAName}",
+          'text-font': [
+            'Open Sans Semibold',
+            'Arial Unicode MS Bold'
+          ],
+          'text-size': {
+            stops: [[11, 12], [14, 16]]
+          },
+          'visibility': getVisibility(layersState.ntas),
+        }
+      });
     }
   
     fetchData();
 
     return () => {
       map.removeLayer('supporting-cds');
+      map.removeLayer('supporting-ntas');
+      map.removeLayer('supporting-cds-labels');
+      map.removeLayer('supporting-ntas-labels');
       map.removeSource('supporting-cds');
+      map.removeSource('supporting-ntas');
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <></>;
+  useEffect(() => {
+    if (map && map.getSource('supporting-cds') && map.getSource('supporting-ntas')) {
+      map.setLayoutProperty('supporting-cds', 'visibility', getVisibility(layersState.cds));
+      map.setLayoutProperty('supporting-ntas', 'visibility', getVisibility(layersState.ntas));
+      map.setLayoutProperty('supporting-cds-labels', 'visibility', getVisibility(layersState.cds));
+      map.setLayoutProperty('supporting-ntas-labels', 'visibility', getVisibility(layersState.ntas));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [layersState])
+
+  return <div
+    className='absolute bottom-0 right-0 m-8 p-2 bg-white rounded-lg shadow cursor-pointer'
+    onMouseOver={() => { setVisibility(true) }}
+    onMouseOut={() => { setVisibility(false) }}
+  >
+    {!supportingLayersToggleVisible && <div>
+      <img src={mapLayers} alt="supporting layers" className='w-8 h-8' />
+    </div>}
+    {supportingLayersToggleVisible && <div className='whitespace-pre-line'>
+      <ul>
+        <li onClick={() => setLayerState({...layersState, ntas: !layersState.ntas })}>Neighborhoods</li>
+        <li onClick={() => setLayerState({...layersState, cds: !layersState.cds })}>Community Districts</li>
+      </ul>
+    </div>}
+  </div>;
 }
