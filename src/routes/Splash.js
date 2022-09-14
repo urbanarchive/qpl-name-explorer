f1aeb2e193217396584caa6bae0befe26cfa787e
Matt Gardner <wilburnforce@gmail.com>
2022-09-14 17:05:50 -0500

    Update Splash.js

--- src/routes/Splash.js
+++ src/routes/Splash.js
@@ -7,7 +7,7 @@ function Splash() {
       Queens Name Explorer
     </h1>
     <p className='font-light text-sm'>
-      Queens Public Library honors the people we serve in the most diverse place in the country and uphold our commitment to inclusion, equity, and free access to information and opportunity for all. We celebrate the Library's rich past and promising future in building resilience and unity in our communities, and as a force for truth and democracy. Click on the pins on the map to see current and past QPL locations and stories submitted by patrons.
+      Welcome to the Queens Name Explorer! In partnership with Urban Archive, the Queens Memory Project presents this interactive map exploring the individuals whose names grace public spaces across the borough. We invite you to join us in finding streets, schools, parks, memorials, and other local spaces that are named after specific people–then learn about their historical significance or contributions to Queens. If you know of other named places we have not added yet to the map, please share a photo of the place and what you know about the individual it’s named after here. Happy exploring!
     </p>
     <Link to='/monuments'>
       <button className='mt-4 w-full p-2 bg-blue-500 rounded-md text-white'>
