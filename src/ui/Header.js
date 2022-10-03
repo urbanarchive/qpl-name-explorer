4cc79adb6432a455b1ca4b8c811f3a6449a59414
Matt Gardner <wilburnforce@gmail.com>
2022-10-03 18:22:58 -0500

    Stop CSS

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -9,7 +9,7 @@ function Header({ locations }) {
 
   return <>
     <div className="flex gap-4 p-4 shadow-lg z-10">
-      <Link to="/" className="flex w-10 h-8 items-center">
+      <Link to="/" className="flex w-14 h-8 items-center">
         <img className='w-8 h-8 bg-red-100 rounded-md' src={qplLogo} alt="QPL Logo" />
       </Link>
       <div className="flex h-8 w-80 items-center">
