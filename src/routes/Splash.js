3a9ccc05e924d0a7d1f88a000b7e1a110cf54ebf
Matt Gardner <wilburnforce@gmail.com>
2022-10-11 13:27:39 -0400

    Melon link

--- src/routes/Splash.js
+++ src/routes/Splash.js
@@ -10,7 +10,7 @@ function Splash() {
       This interactive map explores the individuals whose names grace public spaces across the borough of Queens. If you can add photos or additional information to the entries, or know of other named places we have not added yet to the map, please click the “Add/Edit” button to share what you know. Happy exploring!
     </p>
     <p className='font-light text-xs mt-2'>
-      Major funding for the Queens Name Explorer project was provided by the Mellon Foundation.
+      Major funding for the Queens Name Explorer project was provided by the <a href="https://mellon.org/" target="_blank" rel="noreferrer">Mellon Foundation</a>.
     </p>
     <Link to='/locations'>
       <button className='mt-4 w-full p-2 bg-blue-500 rounded-md text-white'>
