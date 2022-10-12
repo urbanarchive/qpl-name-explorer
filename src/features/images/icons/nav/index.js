d864127ffbced1c2304f673c64a151ece4e7aca6
Matt Gardner <wilburnforce@gmail.com>
2022-10-11 20:53:34 -0400

    Include close button

---
+++ src/features/images/icons/nav/index.js
@@ -0,0 +1,45 @@
+import navAbout from './nav-about.png'
+import navAddWhite from './nav-add-white.png'
+import navBuilding from './nav-building.png'
+import navClose from './nav-close.png'
+import navComment from './nav-comment.png'
+import navHome from './nav-home.png'
+import navLibrary from './nav-library.png'
+import navListWhite from './nav-list-white.png'
+import navList from './nav-list.png'
+import navMapWhite from './nav-map-white.png'
+import navMonument from './nav-monument.png'
+import navPark from './nav-park.png'
+import navSchool from './nav-school.png'
+import navSearchWhite from './nav-search-white.png'
+import navSearch from './nav-search.png'
+import navSelectedTour from './nav-selected-tour.png'
+import navSelected from './nav-selected.png'
+import navShare from './nav-share.png'
+import navStreet from './nav-street.png'
+import navTour from './nav-tour.png'
+
+const NAV_ICONS = {
+  navAbout,
+  navAddWhite,
+  navBuilding,
+  navClose,
+  navComment,
+  navHome,
+  navLibrary,
+  navListWhite,
+  navList,
+  navMapWhite,
+  navMonument,
+  navPark,
+  navSchool,
+  navSearchWhite,
+  navSearch,
+  navSelectedTour,
+  navSelected,
+  navShare,
+  navStreet,
+  navTour,
+};
+
+export default NAV_ICONS;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { MapContext } from './App';
 import { resultFactory } from '../models/location';
 import { DEFAULT_PADDING } from '../ui/Map';
 import { extractlocationIdentifier } from '../models/location';
 import TourView from '../features/Tour';
 import LocationView from '../features/Location';
+import NAV_ICONS from '../features/images/icons/nav';
 
 export const DEFAULT_DETAIL_ZOOM = {
   ...DEFAULT_PADDING,
@@ -28,7 +30,8 @@ function Detail({ locations }) {
 
   if (!location) return;
 
-  return <main>
+  return <main className='relative'>
+    <Link to='/locations' className='absolute right-0 m-4 cursor-pointer'><img alt='close' src={NAV_ICONS.navClose}/></Link>
     {location?.properties?.LOCATION_TYPE === 'ASSET' && <LocationView location={location} map={map}/>}
     {location?.properties?.LOCATION_TYPE === 'TOUR' && <TourView location={location} locations={locations} map={map} />}
   </main>;
