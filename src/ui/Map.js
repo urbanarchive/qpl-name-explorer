c1b16281ec266cf3873f54e2e146c2deaf417970
Matt Gardner <wilburnforce@gmail.com>
2023-03-16 11:37:49 -0400

    Disable hash

--- src/ui/Map.js
+++ src/ui/Map.js
@@ -64,7 +64,6 @@ function Map({
         // this breaks the map because it's pushing the padding too far right
         ...DEFAULT_PADDING,
       },
-      hash: true,
     });
 
     map.on('load', () => {
